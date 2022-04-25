ef72e387915279c4a5c6226512c4acf057b4dd75
Tiziano Marzocchella <tiziano23.marzocchella@gmail.com>
2022-04-25 22:02:38 +0200

    Added biographies and updates

--- donne-e-lotta-per-la-parita-di-genere/index.php
+++ donne-e-lotta-per-la-parita-di-genere/index.php
@@ -17,20 +17,21 @@
 <body>
     <?php require_once "../content/includes/header.html" ?>
 
-    <div class="container-lg mt-4" style="margin-bottom: 5rem">
-        <h1 class="display-2">Donne che hanno lottato per la parità di genere</h1>
-        <div class="article container mb-5" style="max-width:80ch;">
-            <p style="text-align:justify;"></p>
-        </div>
-        <h2>Approfondimenti</h2>
-        <div class="item-list vstack gap-5 my-4">
-            <a class="item hstack gap-3 p-3 shadow" href="donne-e-lotta-per-la-parita-di-genere/olympe-de-gouges">
-                <img class="image img-thumbnail" src="content/pictures/thumb.jpg" alt="Olympe de Gouges" />
-                <div class="vstack gap-2 justify-content-center">
-                    <p class="name h3">Olympe de Gouges</p>
-                    <p class="content"></p>
-                </div>
-            </a>
+    <div class="container-lg mt-4" style="margin-bottom:5rem">
+        <div class="container-fluid" style="max-width:120ch;">
+            <h1 class="display-2 mb-4">Donne che hanno lottato per la parità di genere</h1>
+            <p class="mx-auto fs-5" style="text-align:justify;max-width:80ch">
+            </p>
+            <h2>Approfondimenti</h2>
+            <div class="item-list vstack gap-5 my-4">
+                <a class="item hstack gap-3 p-3 shadow" href="donne-e-lotta-per-la-parita-di-genere/olympe-de-gouges">
+                    <img class="image img-thumbnail" src="content/pictures/thumb.jpg" alt="Olympe de Gouges" />
+                    <div class="vstack gap-2 justify-content-center">
+                        <p class="name h3">Olympe de Gouges</p>
+                        <p class="content"></p>
+                    </div>
+                </a>
+            </div>
         </div>
     </div>
 
