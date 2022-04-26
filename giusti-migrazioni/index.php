292118133cfaa88262c0233da55bc26f83255aa5
Tiziano Marzocchella <tiziano23.marzocchella@gmail.com>
2022-04-26 16:42:08 +0200

    Updates

--- giusti-migrazioni/index.php
+++ giusti-migrazioni/index.php
@@ -20,11 +20,30 @@
     <div class="container-lg mt-4" style="margin-bottom:5rem">
         <div class="container-fluid" style="max-width:120ch;">
             <h1 class="display-2 mb-4">Giusti per le migrazioni</h1>
-            <p class="mx-auto fs-5" style="text-align:justify;max-width:80ch"></p>
+            <div class="mx-auto fs-5" style="text-align:justify;max-width:80ch">
+                <p>
+                    Fin dall'antichità, la storia umana è stata caratterizzata da flussi migratori di massa.
+                    Si stima che gli sfollati, all'interno del loro Paese o all'estero, siano in totale 68,5 milioni, l'1% della popolazione mondiale.
+                    Dei 25,4 milioni di profughi oggi presenti nel mondo, la metà dei profughi proviene solo da tre Paesi. Si tratta della Siria, dove a causa della guerra civile 6.3 milioni di persone hanno lasciato le proprie case, e di Afghanistan e Sud Sudan, abbandonati rispettivamente da 2.6 e 2.4 milioni di abitanti. A questi si devono aggiungere i profughi che negli ultimi mesi hanno abbandonato l'Ucraina, in seguito all'occupazione dell'esercito russo e allo scatenarsi di una nuova guerra sul suolo europeo.
+                    Con il progetto Vivere la storia si è cercato di raccogliere le esperienze dei Giusti nella migrazione, in particolare di coloro che hanno salvato migranti dalla tragedia del 3 ottobre 2013 a Lampedusa, o che si sono adoperati per aiutare chi arrivava soprattutto nel 2015 sull'isola di Lesbo.
+                </p>
+                <h3 class="display-5">La strage del 3 ottobre 2013 a Lampedusa</h2>
+                <p>
+                    La notte del 3 ottobre 2013 nelle acque a largo di Lampedusa un'imbarcazione si è rovesciata al largo dell'Isola dei Conigli, a Lampedusa. A bordo c'erano circa 600 persone, quasi tutte di origine eritrea. Solo 155 di loro si sono salvate: i cadaveri recuperati furono 368, i dispersi 20.
+                    Non è la prima, ma è una delle tragedie più gravi della storia moderna nel Mediterraneo. Il numero delle vittime, la risonanza internazionale dell'accaduto e l'impegno del Comitato 3 ottobre hanno fatto sì che tale data diventasse il simbolo della tragedia dei migranti, tanto da spingere il Parlamento italiano, nel 2015, a istituire per il 3 ottobre una Giornata nazionale in memoria delle vittime dell'immigrazione.
+                    Abbiamo ricordato e onorato chi, come Vito Fiorino, Costantino Baratta, Alganesh Fessaha, si è assunto una responsabilità e ha scelto di aiutare chi fuggiva da guerre e persecuzioni.
+                </p>
+                <h3 class="display-5">L'accoglienza sull'isola di Lesbo</h2>
+                <p>
+                    Tra il 2015 e del 2016, sull'isola greca di Lesbo sono arrivate via mare dalla Turchia 600.000 persone. Sono arrivate soprattutto dalla Siria, fuggendo dalla guerra civile, dall'Isis e dai bombardamenti sulle città.
+                    Lesbo per loro rappresentava il primo passo di un lungo viaggio verso il Nord Europa, che avrebbero proseguito attraverso quella “rotta balcanica” successivamente chiusa dai governi europei.
+                    Lungo le spiagge di Lesbo, a dare loro un primo soccorso e un sorriso capace di ridare dignità e speranza per il futuro, in quei tragici momenti, non c'erano le autorità, ma normali cittadini, abitanti dell'isola e persone accorse da altri Paesi per offrire aiuto.
+                </p>
+            </div>
             <h2 class="display-4">Approfondimenti</h2>
             <div class="item-list vstack gap-5 my-4">
                 <a class="item hstack gap-3 p-3 shadow" href="giusti-migrazioni/costantino-baratta">
-                    <img class="image img-thumbnail" src="content/pictures/baratta.jpg" alt="Costantino Baratta" />
+                    <img class="image img-thumbnail" src="content/pictures/baratta/2.jpg" alt="Costantino Baratta" />
                     <div class="vstack gap-2 justify-content-center">
                         <p class="name h3">Costantino Baratta</p>
                         <p class="content">Il pescatore di Lampedusa che ha salvato 12 migranti eritrei durante la strage del 3 ottobre 2013</p>
