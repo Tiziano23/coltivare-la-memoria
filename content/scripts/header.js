f11cfa17e223f25554f96e8202c0c264ceedbcbf
Tiziano Marzocchella <tiziano23.marzocchella@gmail.com>
2022-04-26 10:45:22 +0200

    General housekeeping

--- content/scripts/header.js
+++ content/scripts/header.js
@@ -1,4 +1,4 @@
 {
-    let activePage = document.querySelector("meta[name=page]")?.content;
-    document.querySelector(`#main-header [data-page=${activePage}]`)?.children[0].classList.add("active");
+    let activePage = document.querySelector("meta[name=activeMenuItem]")?.content;
+    document.querySelector(`#main-header [data-item=${activePage}]`)?.children[0].classList.add("active");
 }
